3e81320f96b417e418e1ba9b80d7b36781b15f23
Miguel <migtam628@gmail.com>
2024-10-10 05:07:04 +0000

    3rd commit

---
+++ scripts/pull_repo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Navigate to the directory where the repository is cloned
+cd /root/node-projects/putio-clone-api 
+
+# Pull the latest chnges from the remote repository
+git pull origin master
