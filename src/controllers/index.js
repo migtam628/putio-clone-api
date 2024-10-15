347242363b3d8e365676eff99fe62f6afb3fae3f
Mig Tam Rod <migtam628@gmail.com>
2024-10-14 22:42:08 -0400

    refactor: Add new controllers for file management

---
+++ src/controllers/index.js
@@ -0,0 +1,31 @@
+import STREAM from './stream.js';
+import DETELE_FILE from './delete_file.js';
+import DELETE_ALL_FILES from './delete_all_files.js';
+import DELETE_DIRECTORY from './delete_directory.js';
+import DELETE_ALL_DIRECTORIES from './delete_all_directories.js';
+import LIST_FILES from './list_files.js';
+import LIST_DIRECTORIES from './list_directories.js';
+import STOP_DOWNLOAD from './stop_download.js';
+import UPLOAD_TORRENT from './upload_torrent.js';
+import HOME from './home.js';
+import FILES from './files.js';
+import ACTIVE_DOWNLOADS from './active_downloads.js';
+import ACTIVE_TORRENTS from './active_torrents.js';
+import STOP_ALL_DOWNLOADS from './stop_all_downloads.js';
+
+export {
+    STREAM,
+    DETELE_FILE,
+    DELETE_ALL_FILES,
+    DELETE_DIRECTORY,
+    DELETE_ALL_DIRECTORIES,
+    LIST_FILES,
+    LIST_DIRECTORIES,
+    STOP_DOWNLOAD,
+    UPLOAD_TORRENT,
+    HOME,
+    FILES,
+    ACTIVE_DOWNLOADS,
+    ACTIVE_TORRENTS,
+    STOP_ALL_DOWNLOADS
+}
