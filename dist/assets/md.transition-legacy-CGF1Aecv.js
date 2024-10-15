0a83b0501225db482e7fe44bb114e1ad82080573
Mig Tam Rod <migtam628@gmail.com>
2024-10-14 22:43:06 -0400

    refactor: Update file controllers for better performance

--- dist/assets/md.transition-legacy-CGF1Aecv.js
+++ dist/assets/md.transition-legacy-CGF1Aecv.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-3hMBUprv.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-DJ1DNEUC.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
